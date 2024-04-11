40202357520f09af21fd789a619dc0f3a4d50586
23qwertyx <251016@edu.p.lodz.pl>
2024-04-11 22:00:00 +0200

    dasdas

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ void usage(char *programName) {
     printf("Usage: %s number1 operation number\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    git commit -m "sraewadsad"
+    git commit -m "sraewadsad"
+    git commit -m "sraewadsad"
     printf("\tsub\n");
     printf("\tmul\n");
     printf("\n");
@@ -19,13 +22,6 @@ int main(int argc, char *argv[]) {
     if (argc == 4) {
         number1 = atoi(argv[1]);
         number2 = atoi(argv[3]);
-
-        if (!strcmp("add", argv[2])) {
-            result = number1 + number2;
-            printf("%d + %d = %d\n", number1, number2, result);
-            return 0;
-
-        }
 
         if (!strcmp("sub", argv[2])) {
             result = number1 - number2;
